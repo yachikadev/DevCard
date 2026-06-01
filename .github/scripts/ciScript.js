c5f3cc23224c16f56ac2bbc6c68498f68815c44c
Harshit Singh Parihar <harsxit04@gmail.com>
2026-06-02 02:05:23 +0530

    fix(ci): expose backendTests output and improve smart test execution (#457)
    * fix: Fixed linting issues
    
    * fix(ci): expose backendTests output and improve smart test execution

--- .github/scripts/ciScript.js
+++ .github/scripts/ciScript.js
@@ -56,7 +56,7 @@ module.exports = async ({ github, context, core }) => {
       backendFiles,
       backendTests,
       mobileFiles,
-      webFiles
+      webFiles, 
     });
 
     core.setOutput(
